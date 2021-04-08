e69c0a2f91fdd328b1438b2650a03ed26e2fcf23
sarboledab <sarboledabotero@gmail.com>
2021-04-08 18:29:49 -0500

    Se configura prod

--- src/app/services/car-service.service.ts
+++ src/app/services/car-service.service.ts
@@ -5,7 +5,6 @@ import { CarI } from '../models/car.interface';
 import { Observable } from 'rxjs';
 import { ResponseI } from '../models/response.interface';
 
-const apiUrl = environment.apiBaseUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +14,7 @@ export class CarServiceService {
   constructor(private httpClient: HttpClient) { }
 
   getCars() {
-      let a =this.httpClient.get(`${apiUrl}/api/v1/cars/`);
+      let a =this.httpClient.get(`http://54.161.207.117:3000/api/v1/cars/`);
       a.subscribe(res => {
         console.log(res);
         
@@ -25,19 +24,19 @@ export class CarServiceService {
   }
 
   saveCar(car:CarI): Observable<ResponseI> { 
-    return this.httpClient.post<ResponseI>(`${apiUrl}/api/v1/cars/`, car);
+    return this.httpClient.post<ResponseI>(`http://54.161.207.117:3000/api/v1/cars/`, car);
   }
 
   getCar(id): Observable<ResponseI> { 
-    return this.httpClient.get<ResponseI>(`${apiUrl}/api/v1/cars/${id}`);
+    return this.httpClient.get<ResponseI>(`http://54.161.207.117:3000/api/v1/cars/${id}`);
   }
 
   updateCar(id, car:CarI): Observable<ResponseI> { 
-    return this.httpClient.put<ResponseI>(`${apiUrl}/api/v1/cars/${id}`, car);
+    return this.httpClient.put<ResponseI>(`http://54.161.207.117:3000/api/v1/cars/${id}`, car);
   }
 
   deleteCar(id): Observable<ResponseI> { 
-    return this.httpClient.delete<ResponseI>(`${apiUrl}/api/v1/cars/${id}`);
+    return this.httpClient.delete<ResponseI>(`http://54.161.207.117:3000/api/v1/cars/${id}`);
   }
 
 
